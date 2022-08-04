6a0f0a9aea44294946a8175f4c2f2312afa6bbc2
Yeonju-Kim <krust@krustui-MacBookPro-8.local>
2022-08-04 11:52:38 +0900

    Revert webpack.config.js entry path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: {
-    index: "./example/src/index.tsx"
+    index: "./src/index.ts"
   },
   output: {
     path: path.resolve(__dirname, "dist"),
